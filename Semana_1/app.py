d769c7ebf3116b707ddaaad6bf63f1ea2a63f5e2
WalterVillatoro1204 <113187380+WalterVillatoro1204@users.noreply.github.com>
2026-09-21 20:19:10 -0600

    Corrección IF

--- Semana_1/app.py
+++ Semana_1/app.py
@@ -23,5 +23,5 @@ modelo_lr.fit(variable_x,variable_y)
 if st.button("Predecir"):
     resultado = modelo_lr.predict([[gasto]])
 
-# Streamlit muestra un mensaje de exito usando success
-st.success(f"Las ventas proyectadas para una inversion de ${gasto} son: ${resultado[0]}")
+    # Streamlit muestra un mensaje de exito usando success
+    st.success(f"Las ventas proyectadas para una inversion de ${gasto} son: ${resultado[0]}")
